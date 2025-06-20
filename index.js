787cac4546539ddf88b683decb58c268fb9d9db0
mrisqid <mrisqid@gmail.com>
2025-06-20 18:33:53 +0700

    setup server and socket.io

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Socket.io</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Server listening on http://localhost:3001');
+});
